f2d1656d38e561d489e9dd232e8edb11304ad7e3
duzenghong <duzenghong@meicai.cn>
2020-08-18 22:41:31 +0800

    HelloWorld

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HelloWord from "./components/HelloWorld";
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <HelloWord />
       </header>
     </div>
   );
